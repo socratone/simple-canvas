64c34c4fcebaa4b3e5bdec23bf6ca5a0dd5e59e5
socratone <gim2origin@gmail.com>
2021-06-29 23:05:22 +0900

    feat: repeat 구현

---
+++ animation/repeat.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let x = 0;
+let direction = 'right';
+
+function draw() {
+  if (direction === 'right') x += 5;
+  else x += -5;
+  if (x >= 200) direction = 'left'
+  if (x <= 0) direction = 'right'
+
+  ctx.clearRect(0, 0, 300, 300)
+  ctx.fillRect(x, 100, 100, 100);
+  window.requestAnimationFrame(draw)
+}
+
+window.requestAnimationFrame(draw)
